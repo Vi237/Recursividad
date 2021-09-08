18dd2b231b9e58dccf978c4854e2ceaf4cebe584
vi237 <vicaerosandoval@gmail.com>
2021-09-08 09:50:44 -0400

    Recursividad

---
+++ src/Ejercicios.java
@@ -0,0 +1,29 @@
+public class Ejercicios {
+    public static int factorial(int n){
+        int factorial;
+        if (n==1){
+            //caso base
+            factorial=n;
+        }else {
+            factorial=n*factorial(n-1);
+        }
+
+        return factorial;
+    }
+    //MCD, algoritmo de Eclides---(a=412 y b=184)
+    public static int mcd(int a, int b){
+        int mcd;
+        if(a==b){
+            //caso base
+            mcd =b;
+        }else {
+            if(a>b){
+                a=a-b;
+            }else {
+                b=b-a;
+            }
+            mcd=mcd(a, b);
+        }
+        return mcd;
+    }
+}
